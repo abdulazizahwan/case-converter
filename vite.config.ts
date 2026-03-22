66dbd5d74a6056916694d98639a72ca6ee2212d5
Abdul Aziz Ahwan <abdulazizahwan97@gmail.com>
2026-03-22 20:07:06 +0700

    fix: update base path in Vite configuration for correct deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    base: "/case-converter/",
+    base: "./",
     plugins: [react(), tailwindcss()],
     define: {},
     resolve: {
